69be08f70427df50dba94a2c4e386948f6e88348
SWAYANGPRAVA <biswalswayangprava@gmail.com>
2024-02-22 09:00:52 +0530

    init

---
+++ Python-2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first integer (a): "))
+b = int(input("Enter the second integer (b): "))
+
+if a < b:
+    print("a is smaller than b")
+elif a > b:
+    print("a is larger than b")
+else:
+    print("a is equal to b")
