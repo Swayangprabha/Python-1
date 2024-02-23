9d8f0e6639c865867fbba76701789be91c2f9386
SWAYANGPRAVA <biswalswayangprava@gmail.com>
2024-02-23 08:59:52 +0530

    init

---
+++ Python-7.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the number of schoolchildren: "))
+k = int(input("Enter the number of apples: "))
+remainder = k % n
+print("Number of apples remaining in the basket:", remainder)
+
